4e244389f908a3166ee4cadb421dd1a0fcf4724a
ahmada <amethgaye1401@gmail.com>
2025-11-16 00:45:57 +0000

    real data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 
 // import events from "@/lib/constants";
 import ExploreBtn from "@/components/ExploreBtn"
-import EventCards from "@/components/EventCards"
+import EventCard from "@/components/EventCard"
+import {IEvent} from "@/database";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 async function HomePage() {
-  
+    const response = await fetch(`${BASE_URL}/api/events`, { next: { revalidate: 60 }});
+    const {events} = await response.json();
     return (
       <section>
         <h1 className= "text-center">The hub for Every Dev <br/> Event You Can&apos;t Miss</h1>
@@ -14,7 +17,13 @@ async function HomePage() {
 
         <div className="mt-20 space-y-7">
           <h3>Featured Events</h3>
-            <EventCards />
+            <ul className="events">
+              {events && events.length > 0 && events.map((event : IEvent ) => (
+                <li className="list-none" key={event.title}>
+                  <EventCard {...event} />
+                </li>
+              ))}
+          </ul>
         </div>
       </section>
     )
